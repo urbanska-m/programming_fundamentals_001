c7c80e5771ffd4e5bed891a05240964cde5a7a16
Monika Urbanska <monika.urbanska1@gmail.com>
2019-11-25 09:48:22 +0000

    Day 1 learning fundamentals with book shop

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,40 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, secondName) {
+    // return firstName + " " + secondName;
+    return `${firstName} ${secondName}`;
+}
+
+function makeHalfPrice(price) {
+    return price / 2;
+}
+
+function countBooks(bookList) {
+    return bookList.length;
+}
+
+function isInStock(book) {
+    const quantity = book.quantity;
+    if(quantity >= 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+
+function getTotalOrderPrice(price, quantity) {
+    return 1.2 * (price * quantity);
+}
+
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,66 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
+});
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+
+// new test to makeHalfPrice function
+describe("utils.makeHalfPrice", () => {
+  test("returns 15 when passed 30", () => {
+    expect(utils.makeHalfPrice(30)).toBe(15);
+  });
+});
+
+// new test to countBooks function
+describe("utils.countBooks", () => {
+  test("count array of two books and return 2", () => {
+    expect(utils.countBooks(['Moby Dick', 'Sense and Sensibility'])).toBe(2);
+  });
+
+  test("count array of three books and return 3", () => {
+    expect(utils.countBooks(['Great Expectations', 'It', 'Sapiens'])).toBe(3);
+  });
+});
+
+describe("utils.isInStock", () => {
+  test("returns true if the title is in stock", () => {
+  const book = {
+    title: "The Stone Diaries",
+    author: "Carol Shields",
+    yearOfPublication: 1993,
+    quantity: 9
+  };
+  expect(utils.isInStock(book)).toBe(true);
+  });
+
+  test("returns false if the title is not in stock", () => {
+    const book = {
+      title: "Pet Sematary",
+      author: "Stephen King",
+      yearOfPublication: 1983,
+      quantity: 0
+    };
+    expect(utils.isInStock(book)).toBe(false);
+    });
+});
+
+// return price multiplied by quantity, with added 20% VAT
+describe("utils.getTotalOrderPrice", () => {
+  test("return 96 when passed 8, 10", () => {
+    expect(utils.getTotalOrderPrice(8, 10)).toBe(96);
+  });
+
+  test("return 54 when passed 15, 3", () => {
+    expect(utils.getTotalOrderPrice(15, 3)).toBe(54);
+  });
 });
